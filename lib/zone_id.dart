da9811a61f88d50accdbf29ead237a71dbe11f11
Philipp Schiffmann <philippschiffmann93@gmail.com>
2019-08-19 09:52:44 +0200

    WIP

--- lib/zone_id.dart
+++ lib/zone_id.dart
@@ -1,43 +1,65 @@
 import 'dart:async';
 
-const someToken = Token<String>('someToken');
+final someToken = Token<String>('someToken');
 
 void main() {
-  provide(someToken, 'hello world', () {
+  provideSingle(someToken, 'hello world', () {
     final s = inject(someToken);
     print(s);
   });
-  provideAll({someToken: 'foo'}, () {
+  provide({someToken: 'foo'}, () {
     print(inject(someToken));
   });
 }
 
-class Token<T> {
-  const Token(this._identifier)
-      : _defaultValue = null,
-        _hasDefaultValue = false;
-  const Token.withDefaultValue(this._identifier, [this._defaultValue])
-      : _hasDefaultValue = true;
+class _NoValue {}
 
-  final dynamic _identifier;
-  final T _defaultValue;
-  final bool _hasDefaultValue;
+class Token<T> {
+  Token(this._debugName) : _defaultValue = _NoValue;
+  Token.withDefault(this._debugName, T defaultValue)
+      : _defaultValue = defaultValue;
+
+  final dynamic _debugName;
+  final Object _defaultValue;
 
   T _cast(dynamic v) => v as T;
 
   @override
-  String toString() => 'Token($_identifier)';
+  String toString() => 'Token($_debugName)';
 }
 
-R provide<T, R>(Token<T> token, T value, R Function() f) =>
+R provideSingle<T, R>(Token<T> token, T value, R Function() f) =>
     runZoned(f, zoneValues: {token: value});
 
-R provideAll<R>(Map<Token, dynamic> values, R Function() f) =>
-    runZoned(f, zoneValues: values..forEach((t, v) => t._cast(v)));
+R provide<R>(Map<Token, dynamic> values, R Function() f) =>
+    runZoned(f, zoneValues: {
+      _Injector: _Injector(values.map((t, v) => t._cast(v))),
+    });
 
-T inject<T>(Token<T> token) {
-  final zoneValue = Zone.current[token];
-  if (zoneValue != null) return token._cast(zoneValue);
-  if (token._hasDefaultValue) return token._defaultValue;
-  throw Exception();
+T inject<T>(Token<T> token) =>
+    ((Zone.current[_Injector] as _Injector) ?? _Injector.root).get(token);
+
+class _Injector {
+  _Injector(this.values);
+  const _Injector.empty() : values = const {};
+
+  final Map<Token, Object> values;
+
+  T get<T>(Token<T> token) {
+    if (values.containsKey(token)) return token._cast(values[token]);
+    final parent = Zone.current.parent[_Injector] as _Injector;
+    if (parent != null) return parent.get(token);
+    if (token._defaultValue != _NoValue) return token._defaultValue;
+    throw MissingDependencyException(token);
+  }
+
+  static const root = _Injector.empty();
+
+  static _Injector forZone(Zone zone) =>
+      (zone[_Injector] as _Injector) ?? const _Injector.empty();
+}
+
+class MissingDependencyException implements Exception {
+  MissingDependencyException(this.token);
+  final Token token;
 }
